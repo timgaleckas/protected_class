9b590fb7cde52897177d35dccb64504015af3e61
Tim Galeckas <tim@galeckas.com>
2011-04-19 16:23:38 -0500

    make Object protected in the same way our individual classes are

--- go.rb
+++ go.rb
@@ -1,7 +1,8 @@
 require 'lib/protected_class'
 require 'lib/can_touch_me'
 require 'lib/cant_touch_me'
-Object.freeze
+Object.send :include, ProtectedClass
+
 Dir.glob('lib/user_defined/*.rb').each{|file|require file}
 
 raise 'fail' unless CantTouchMe.new.do_it=="yes"
