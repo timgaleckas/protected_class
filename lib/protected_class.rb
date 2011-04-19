8f6971fc2fed56d80da1311dfa08e93d08ddd250
Tim Galeckas <tim@galeckas.com>
2011-04-19 15:36:57 -0500

    two more vectors sealed. Thanks svesely and tjbladez

--- lib/protected_class.rb
+++ lib/protected_class.rb
@@ -17,4 +17,8 @@ module ProtectedClass
   def __send__(*args); public_send(*args); end
   def instance_variable_get(*args); __bail__; end
   def instance_variable_set(*args); __bail__; end
+  def const_set(*args); __bail__; end
+  def const_get(*args); __bail__; end
 end
+def Object.const_set(*args);    exec('echo Cheating detected'); end
+def Object.remove_const(*args); exec('echo Cheating detected'); end
